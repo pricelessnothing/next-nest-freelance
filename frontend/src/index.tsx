d29b86eb1c6f9b7569598a4c06dec3aff2b09cff
daniil.egorov <daniil.egorov@spbpu.com>
2021-08-25 13:48:23 +0300

    frontend init

---
+++ frontend/src/index.tsx
@@ -0,0 +1,5 @@
+import ReactDOM from 'react-dom'
+
+import { HomePage } from './views/Home'
+
+ReactDOM.render(<HomePage />, document.getElementById('app'))
---
+++ frontend/webpack.config.js
@@ -0,0 +1,59 @@
+const path = require('path')
+
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src', 'index.tsx'),
+
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+
+  mode: process.env.MODE ?? 'development',
+
+  module: {
+    rules: [
+      {
+        test: /\.[jt]sx?$/,
+        use: ['babel-loader'],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.sass$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+        type: 'asset/inline',
+      },
+    ],
+  },
+
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.jsx'],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src', 'index.html'),
+    }),
+    new CleanWebpackPlugin(),
+  ],
+
+  devServer: {
+    port: 3001,
+    hot: true,
+    compress: true,
+    open: false,
+  },
+}
